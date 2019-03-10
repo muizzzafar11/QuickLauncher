0793e78f9bd01afd0c5d62adf54ce9e4740f02f9
muizzzafar11 <43075405+muizzzafar11@users.noreply.github.com>
2019-03-09 23:03:58 -0500

    Added connection of the button to google docs

--- app/src/main/java/com/example/quicklauncher/MainActivity.java
+++ app/src/main/java/com/example/quicklauncher/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.quicklauncher;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,16 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //if the calculator button is pressed then perform the following function
+        //attempts to access an activity inside the app
         Button Calculator = (Button) findViewById(R.id.Calculator);
         Calculator.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent secondActivity = new Intent(getApplicationContext(),SecondActivity.class);
                 //how to shift the screen to the other second activity
-
                 startActivity(secondActivity);
-
             }
         });
+        //if the google button id pressed then perform the following function
+        //attempts to access an activity outside the app
+        Button google = findViewById(R.id.Search);
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String GoogleUrl = "https://www.google.com/docs/about/";
+                Uri WebAdress = Uri.parse(GoogleUrl);
+                Intent gotoGoogle = new Intent(Intent.ACTION_VIEW,WebAdress);
+                startActivity(gotoGoogle);
+            }
+        });
+
     }
 }
